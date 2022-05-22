203a511e1bb8cb9773d7ac3ffd54726ccba75fc4
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-22 18:00:43 +0600

    Generate image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_22_095948) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_22_115956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "image"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_images_on_project_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -21,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_22_095948) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "images", "projects"
 end
